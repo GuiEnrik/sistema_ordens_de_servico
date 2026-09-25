7dc24bbac78a43c0442ab8cc93409b5ee69ebd8a
GuiEnrik <guienrik@gmail.com>
2026-09-25 18:26:44 -0300

    Configura conexão PDO com MySQL

--- conecta.php
+++ conecta.php
@@ -1,8 +1,18 @@
 <?php
 try {
-    $nome_banco = '';
-    $conexao = new PDO('sqlite:'. $nome_banco);
+    $host = 'localhost';
+    $banco = 'sistema_ordens_servico';
+    $usuario = 'root';
+    $senha = '';
+
+    $conexao = new PDO(
+        "mysql:host=$host;dbname=$banco;charset=utf8mb4",
+        $usuario,
+        $senha
+    );
+
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
